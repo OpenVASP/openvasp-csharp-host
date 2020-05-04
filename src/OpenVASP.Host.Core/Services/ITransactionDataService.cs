ed0900033fc85b73c85e1f6b4d1fbe8ab0ffa224
Sergey Nesterov <sergey.nesterov@lykke.com>
2020-05-04 16:56:38 +0300

    Added interfaces for services

---
+++ src/OpenVASP.Host.Core/Services/ITransactionDataService.cs
@@ -0,0 +1,32 @@
+﻿using OpenVASP.Messaging.Messages;
+using OpenVASP.Messaging.Messages.Entities;
+using PlaceOfBirth = OpenVASP.Host.Core.Models.PlaceOfBirth;
+using PostalAddress = OpenVASP.Host.Core.Models.PostalAddress;
+using Transaction = OpenVASP.Host.Core.Models.Transaction;
+
+namespace OpenVASP.Host.Core.Services
+{
+    public interface ITransactionDataService
+    {
+        (Transaction, Originator) GenerateTransactionData(
+            string originatorFullName,
+            string originatorVaan,
+            PlaceOfBirth originatorPlaceOfBirth,
+            PostalAddress originatorPostalAddress,
+            string beneficiaryFullName,
+            string beneficiaryVaan,
+            VirtualAssetType asset,
+            decimal amount,
+            NaturalPersonId[] naturalPersonIds,
+            JuridicalPersonId[] juridicalPersonIds,
+            string bic);
+
+        VirtualAssetsAccountNumber CreateVirtualAssetsAccountNumber(string beneficiaryVaan);
+
+        void FillTransactionData(Transaction transaction, TransferRequestMessage message);
+
+        Originator GetOriginatorFromTx(Transaction transaction);
+
+        Beneficiary GetBeneficiaryFromTx(Transaction transaction);
+    }
+}
