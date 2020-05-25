84640a158d9b9a9b6060acf44fdf10858f0247a9
Sergey Nesterov <sergey.nesterov@lykke.com>
2020-05-25 22:34:52 +0300

    Added autoconfirmed vasp codes feature

---
+++ src/OpenVASP.Host.Services/VaspCodeManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenVASP.Host.Core.Services;
+
+namespace OpenVASP.Host.Services
+{
+    public class VaspCodeManager : IVaspCodeManager
+    {
+        private readonly HashSet<string> _vaspCodes = new HashSet<string>();
+
+        public VaspCodeManager(IEnumerable<string> vaspCodes)
+        {
+            foreach (var vaspCode in vaspCodes)
+            {
+                _vaspCodes.Add(vaspCode.ToLower());
+            }
+        }
+
+        public List<string> GetAutoConfirmedVaspCodes()
+        {
+            return _vaspCodes.ToList();
+        }
+
+        public bool IsAutoConfirmedVaspCode(string vaspCode)
+        {
+            return _vaspCodes.Contains(vaspCode.ToLower());
+        }
+    }
+}
